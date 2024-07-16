36d25f6365c1d7529dafa77d7780f1a8cbff67b3
NNliu <tb39123@gmail.com>
2024-07-17 07:40:54 +0800

    Initial Commit

---
+++ TrendyChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrendyChat
+//
+//  Created by Ning Liu on 2024/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrendyChat/TrendyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendyChatApp.swift
+//  TrendyChat
+//
+//  Created by Ning Liu on 2024/7/17.
+//
+
+import SwiftUI
+
+@main
+struct TrendyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
